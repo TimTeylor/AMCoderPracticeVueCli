9a2a9fd652a6e7104e86884664f86b1582ec7efe
TimTeylor <timteillor@gmail.com>
2020-09-05 16:05:21 +0300

    router, add page

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,44 @@
 <template>
-  <div>
-    <h1>Hello World</h1>
-    <h2>{{ cheese }}</h2>
-    <HelloWorld :cheese="cheese"/>
-    <button @click="handleClick">Click Me</button>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> |
+      <router-link to="/another">Another</router-link>
+    </div>
+    <Header/>
+    <router-view/>
+    <Footer/>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld"
+import Header from './components/Header'
+import Footer from './components/Footer'
 export default {
   name: 'App',
-  data() {
-    return {
-      cheese: "gouda"
-    }
-  },
-  components: {HelloWorld},
-  methods: {
-    handleClick() {
-      this.cheese = "munster"
-    }
-  },
+  components: { Header, Footer }
 }
 </script>
 
 <style>
-h1 {
-  color: green;
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>This is the Header</h1>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
